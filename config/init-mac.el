331ad208bebb578751d002175f29656af3dcebab
Brooks <kbrooks@SEAM3012924B.local>
2016-02-01 18:43:56 -0800

    Adding exec-path-from-shell-initialize to init-mac.el

--- config/init-mac.el
+++ config/init-mac.el
@@ -1,4 +1,6 @@
 ;; There is an issue with visual-bell on El Capitan. We need to change the ring-bell-function.
 (setq ring-bell-function (lambda () (message "*woop*")))
 
+(exec-path-from-shell-initialize)
+
 (provide 'init-mac)
