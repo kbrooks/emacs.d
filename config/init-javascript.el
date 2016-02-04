a106de8962c1324c82f7168e2286373fa41eeb67
Kyle Brooks <kyletbrooks@gmail.com>
2016-02-03 18:49:25 -0800

    Adding jsxhint flycheck to jsx-mode

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -1,13 +1,15 @@
 ;;;; react
 (require-package 'jsx-mode)
 (require-package 'web-mode)
+(require-package 'flycheck)
 
 ;https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking/
-(flycheck-define-checker jsxhint-checker
-  "A JSX syntax and style checker based on JSXHint."
-  :command ("jsxhint" source)
-  :error-patterns ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-  :modes (jsx-mode web-mode))
+(after 'flycheck
+  (flycheck-define-checker jsxhint-checker
+    "A JSX syntax and style checker based on JSXHint."
+    :command ("jsxhint" source)
+    :error-patterns ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+    :modes (jsx-mode web-mode)))
 
 (add-hook 'jsx-mode-hook
           (lambda ()
