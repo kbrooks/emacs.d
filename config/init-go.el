0bee2c9263bdf790b8ee8307cce1640f3608932f
Kyle Brooks <kyletbrooks@gmail.com>
2016-02-04 16:18:03 -0800

    Deleting unused files

--- config/init-go.el
+++
@@ -1,5 +0,0 @@
-; http://dominik.honnef.co/posts/2013/03/writing_go_in_emacs/
-(require-package 'go-mode)
-
-
-(provide 'init-go)
--- config/init-workgroups.el
+++
@@ -1,9 +0,0 @@
-(require-package 'workgroups2)
-
-(workgroups-mode 1)
-
-
-(wg-find-session-file (concat user-emacs-directory ".cache/workgroups/"))
-
-
-(provide 'init-workgroups)
