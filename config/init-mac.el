d362a3d29afad983488be8bc2fc518574afa558e
Kyle Brooks <kyletbrooks@gmail.com>
2016-02-18 11:08:11 -0800

    Making init-mac and init-windows-nt only execute on their respective OSes

--- config/init-mac.el
+++ config/init-mac.el
@@ -1,12 +1,13 @@
 ;; MAC OS X SPECIFIC CONFIGURATION OPTIONS.
 
-;; There is an issue with visual-bell on El Capitan. We need to change the ring-bell-function.
-(setq ring-bell-function (lambda () (message "*woop*")))
-
-;; mac doesnt load path correctly
-(exec-path-from-shell-initialize)
-
-;; mac's ls doesn't have --dired
-(setq dired-use-ls-dired nil)
+(if (eq system-type 'darwin)
+    (progn
+      ;; There is an issue with visual-bell on El Capitan.
+      ;; We need to change the ring-bell-function.
+      (setq ring-bell-function (lambda () (message "*woop*")))
+      ;; mac doesnt load path correctly
+      (exec-path-from-shell-initialize)
+      ;; mac's ls doesn't have --dired
+      (setq dired-use-ls-dired nil)))
 
 (provide 'init-mac)
--- config/init-windows-nt.el
+++ config/init-windows-nt.el
@@ -1,21 +1,23 @@
 ;;;; WINDOWS SPECIFIC CONFIGURATION OPTIONS
 
 ;; SYMANTEC!!! *shakes fist*
-(setq w32-get-true-file-attributes nil)
+(if (eq system-type 'windows-nt)
+    (progn 
+      (setq w32-get-true-file-attributes nil)
 
-;; projectile hack
-(with-eval-after-load 'projectile
-  (setq projectile-indexing-method 'alien))
+      ;; projectile hack
+      (with-eval-after-load 'projectile
+        (setq projectile-indexing-method 'alien))
 
-(defun my-remove-dos-eol ()
-  "Do not show ^M in files containing mixed UNIX and DOS line endings."
-  (interactive)
-  (setq buffer-display-table (make-display-table))
-  (aset buffer-display-table ?\^M []))
+      (defun my-remove-dos-eol ()
+        "Do not show ^M in files containing mixed UNIX and DOS line endings."
+        (interactive)
+        (setq buffer-display-table (make-display-table))
+        (aset buffer-display-table ?\^M []))
 
-;; Remove ^M from clojure repl in windows
-(with-eval-after-load 'cider-mode
-  (add-hook 'cider-repl-mode-hook 'my-remove-dos-eol))
+      ;; Remove ^M from clojure repl in windows
+      (with-eval-after-load 'cider-mode
+        (add-hook 'cider-repl-mode-hook 'my-remove-dos-eol))))
 
 (provide 'init-windows-nt)
 
