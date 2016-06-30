628164a3a6059edc49e3763d4a6b386fac0bc39d
Kyle Brooks <kyletbrooks@gmail.com>
2016-06-30 09:45:13 -0700

    Fixing yasnippet: adding yas-recompile-all to startup

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -1,11 +1,6 @@
 (require-package 'yasnippet)
 
-;; (setq yas-fallback-behavior 'return-nil)
-;; (setq yas-also-auto-indent-first-line t)
-;; (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
-
-(setq yas-snippet-dirs
-      `(,(concat user-emacs-directory "snippets")))
+(yas-recompile-all)
 
 (yas-global-mode 1)
 
