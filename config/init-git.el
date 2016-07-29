9abbb74fc0e28069fdb50d1112cdfed6e5aa7cec
Kyle Brooks <kyletbrooks@gmail.com>
2016-07-28 17:49:33 -0700

    Adding git-link handler for imshealth gitlab

--- config/init-git.el
+++ config/init-git.el
@@ -7,4 +7,18 @@
 
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
+(defun my-git-link-imshealth-gitlab (hostname dirname filename branch commit start end)
+  (format "https://%s/%s/blob/%s/%s#%s"
+	  (substring hostname 0 -5)
+          dirname
+	  (or branch commit)
+	  filename
+	  (if end
+	      (format "L%s-%s" start end)
+	    (format "L%s" start))))
+
+(add-to-list 'git-link-remote-alist
+    '("gitlab.imshealth.com:2222" my-git-link-imshealth-gitlab))
+
 (provide 'init-git)
+
