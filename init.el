20fbf3e9481b14bfb94f878892bbeaa654adb1a2
Kyle Brooks <kylbr@amazon.com>
2016-11-03 00:07:35 +0000

    Marmalade is a bad influence and must be cast out

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
   
   ;;; packages
   (require 'package)
-  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
