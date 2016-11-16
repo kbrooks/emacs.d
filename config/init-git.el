c85940a334b98e0f34f90d4ee9492d46ee795af6
Kyle Brooks <kylbr@amazon.com>
2016-11-16 01:02:15 +0000

    Adding git-link for code.amazon.com

--- config/init-git.el
+++ config/init-git.el
@@ -7,19 +7,20 @@
 
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
-(defun my-git-link-imshealth-gitlab (hostname dirname filename branch commit start end)
-  (format "https://%s/%s/blob/%s/%s#%s"
-	  hostname
-          dirname
-	  (or branch commit)
-	  filename
-	  (if end
-	      (format "L%s-%s" start end)
-	    (format "L%s" start))))
+;;; generates link to code.amazon.com
+(defun my-git-link-amazon-gitlab (hostname dirname filename branch commit start end)
+  (format "https://code.amazon.com/packages/%s/blobs/%s/--/%s#%s"
+          (if (string= (substring dirname 0 4) "pkg/")
+              (substring dirname 4)
+            dirname)
+          (or branch commit)
+          filename
+          (if end
+              (format "L%s-%s" start end)
+            (format "L%s" start))))
 
 (with-eval-after-load 'git-link
  (add-to-list 'git-link-remote-alist
-    '("gitlab.imshealth.com:2222" my-git-link-imshealth-gitlab)))
+    '("git.amazon.com" my-git-link-amazon-gitlab)))
 
 (provide 'init-git)
-
